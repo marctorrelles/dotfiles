513a2fae3fcc0b31c9d6ad69f946dc3f0ec7e697
Marc Torrelles <marctorrelles@gmail.com>
2022-06-02 08:30:34 +0200

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# bashrc
-echo 'copying .bash_profile'
-cp "${PWD}/.bash_profile" ~/.bash_profile
-echo 'sourcing .bash_profile'
-source ~/.bash_profile
+files=".bash_profile .gitconfig .tmux.conf"
 
-## git
-echo 'copying .gitconfig'
-cp "${PWD}/.gitconfig" ~/.gitconfig
-
-# TMUX
-echo 'copying .tmux'
-cp "${PWD}/.tmux.conf" ~/.tmux.conf
-
-
+for file in $files
+do
+  if [ -L $HOME/$file ]; then
+    unlink $HOME/$file
+  fi
+  ln -s $HOME/.dotfiles/$file $HOME
+done
